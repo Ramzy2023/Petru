1cd3cba7e650440d38e79deee35ef6286f560486
Ramzy2023 <lordram.mondano@gmail.com>
2024-11-08 19:19:04 +0800

    added Division

--- myprog/myapp/urls.py
+++ myprog/myapp/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
- 
+    path('',views.main,name='main'),
+    path('division/',views.division,name='division'),
 ]
--- myprog/myapp/views.py
+++ myprog/myapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import AssignTravel,Division,Employee,NatureOfTravel,Office,TravelOrder,Position
 
 # Create your views here.
+def main(request):
+    return render(request,'myapp/main.html')
+
+def division(request):
+    division = Division.objects.all()
+
+    if request.method == "POST":
+        division_name = request.POST.get("division_name")
+        Division.objects.create(
+            division_name = division_name
+        )
+    
+    context = {"division_list":division}
+    return render(request,'myapp/division.html',context)
+
